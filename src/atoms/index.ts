0a4f4eb11c3fad307237933019abd828951aafb5
valentina.marrazzo <valemarr99@gmail.com>
2024-12-15 01:03:56 +0100

    fix: removed unused library imports and utils

--- src/atoms/index.ts
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import { RealTimeData } from "@/lib/types";
-import { atom } from "jotai";
-
-export const realTimeData = atom<RealTimeData>({} as RealTimeData);
--- src/lib/ProvidersAggrergator.tsx
+++ src/lib/ProvidersAggrergator.tsx
@@ -3,7 +3,6 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider as StoreProvider } from "jotai";
 import { ReactNode } from "react";
 import theme from "./theme";
 
@@ -12,9 +11,7 @@ export const ProvidersAggrergator = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <AppRouterCacheProvider>
-        <ThemeProvider theme={theme}>
-          <StoreProvider>{children}</StoreProvider>
-        </ThemeProvider>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </AppRouterCacheProvider>
     </QueryClientProvider>
   );
